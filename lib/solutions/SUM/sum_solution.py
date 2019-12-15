ad5d3bb03c98463dd67d00d4dc68d7456a32bd1f
sbx_user1051 <sbx_user1051@169.254.200.77>
2019-12-15 12:19:46 +0000

    Sun Dec 15 12:19:46 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,12 +1,12 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def get_total_value(x, y):
-	'''
-	Add two numbers x and y
-	:param: x: first number
-	:param: y: second number
-	:return: total value after adding x and y
-	:rype: int
-	'''
+    '''
+    Add two numbers x and y
+    :param: x: first number
+    :param: y: second number
+    :return: total value after adding x and y
+    :rype: int
+    '''
     return 10
 
 
