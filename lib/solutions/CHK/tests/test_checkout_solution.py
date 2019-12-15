3efca3ed3c9acfc078221fcbcfd0dbbcbbd16c35
sbx_user1051 <sbx_user1051@169.254.200.77>
2019-12-15 13:51:00 +0000

    Sun Dec 15 13:51:00 UTC 2019

--- lib/solutions/CHK/tests/test_checkout_solution.py
+++ lib/solutions/CHK/tests/test_checkout_solution.py
@@ -9,3 +9,7 @@ class TestCheckout():
         Test return value
         """
         assert checkout("A, A") == 100
+
+    # def test_return_val_with_diffrent_items(self):
+    #     assert checkout("A, B, C") == 100
+
