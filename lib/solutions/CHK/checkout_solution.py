14a6a152b798605d5a12ec70c51ee1f06fc581ef
sbx_user1051 <sbx_user1051@169.254.200.77>
2019-12-15 13:36:00 +0000

    Sun Dec 15 13:36:00 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+	items = skus.split(',')
+	item_dic = {}
+	for x in items:
+		if x in item_dic:
+			item_dic[x] += 1
+		else:
+			item_dic[x] = 1
     return 100
+
 
