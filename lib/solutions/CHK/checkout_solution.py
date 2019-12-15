cafd99fdb6fdb7f6e44d68fcec48748534e3d348
sbx_user1051 <sbx_user1051@169.254.200.77>
2019-12-15 13:42:00 +0000

    Sun Dec 15 13:42:00 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,25 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-	items = skus.split(',')
-	item_dic = {}
-	for x in items:
-		if x in item_dic:
-			item_dic[x] += 1
-		else:
-			item_dic[x] = 1
-    return 100
+    items = skus.split(',')
+    item_dic = {}
+    for x in items:
+        if x in item_dic:
+            item_dic[x] += 1
+        else:
+            item_dic[x] = 1
+
+    total = 0
+
+    for key, value in item_dic.items():
+        if key == 'A':
+            total += 50
+        elif key == 'B':
+            total += 30
+        elif key == 'C':
+            total += 20
+        elif key == 'D':
+            total += 15
+
+    return total
+
