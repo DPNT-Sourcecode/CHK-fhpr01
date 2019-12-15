902aaa23bb208a8dff9301857cf6cfcbc2e4c7fa
sbx_user1051 <sbx_user1051@169.254.200.77>
2019-12-15 12:59:29 +0000

    Sun Dec 15 12:59:29 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,7 +4,10 @@
 # friend_name = unicode string
 def hello(friend_name):
     """
-    Return hello messsage
+    Return hello message for a friend
+    :param: friend_name: the person's name
+    :return: String containing the message
     """
     return f'Hello, {friend_name}!'
+
 
